38eb7dfff5d2d3a364869425216b8c7acf153ce9
ross96D <abelaross96@gmail.com>
2024-01-01 12:43:59 -0500

    add some abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -2,3 +2,11 @@ function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
 end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+abbr --add S --position command sudo
+abbr --add SS --position command "sudo systemctl"
+
+function last_history_item
+    echo $history[1]
+end
+abbr -a !! --position anywhere --function last_history_item
