ab44b2bc6471c05af087d155181d4ef9eb54c62d
ross96D <abelaross96@gmail.com>
2024-01-02 18:44:58 -0500

    add git abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -11,8 +11,10 @@ function last_history_item
 end
 abbr -a !! --position anywhere --function last_history_item
 
+abbr -a g --position command "git "
 abbr -a gs --position command "git status"
 abbr -a gp --position command "git push"
 abbr -a ga --position command "git add"
+abbr -a gc --position command --set-cursor "git commit -m \"%\""
 abbr -a gac --position command --set-cursor "git add . && git commit -m \"%\""
 abbr -a gaca --position command "git add . && git commit --amend"
